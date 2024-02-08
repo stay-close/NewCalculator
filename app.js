86f2c0f579f61a20a7bfbb867f8949be78b2174a
stay-close <swapansapu2@gmail.com>
2024-02-09 02:18:34 +0600

    logic added for operators

--- app.js
+++ app.js
@@ -5,32 +5,73 @@ const keys=document.querySelectorAll('.key')
 let input=''
 
 keys.forEach(key=>{
-  let value=key.dataset.key;
+  let lastInputValue=key.dataset.key;
 
   key.addEventListener('click',()=>{
 
-    if(value=='clearAll'){
+    if(lastInputValue=='clearAll'){
 
       input='';
       outputScreen.innerHTML=''
       inputScreen.innerHTML=''
 
-    }else if(value=='backspace'){
+    }else if(lastInputValue=='backspace'){
 
       input=input.slice(0,-1)
       inputScreen.innerHTML=input;
 
-    }else if(value=='='){
+    }else if(lastInputValue=='='){
       console.log(input)
       
       let result=eval(input)
      outputScreen.innerHTML=result;
      
 
-    }else{
-      input+=value;
+    }else if(lastInputValue=='brackets'){
+
+      if(
+        input.indexOf('(')==-1 ||
+        input.indexOf('(')!=-1 &&
+        input.indexOf(')')!=-1 &&
+        input.lastIndexOf('(')<input.lastIndexOf(')')
+
+      ){
+        input+='('
+      }else if(
+        input.indexOf('(')!=-1 &&
+        input.indexOf(')')==-1 ||
+        input.lastIndexOf('(')>input.lastIndexOf(')') 
+      ){
+        input+=')'
+      }
       inputScreen.innerHTML=input;
+
+    }else{
+     if(validateInput(lastInputValue)){
+      input+=lastInputValue;
+      inputScreen.innerHTML=input;
+     }
     }
 
   })
 })
+
+
+function validateInput(inputValue){
+  let lastInput=input.slice(-1);
+  const operators=['+','-','*','/'];
+
+  if(lastInput=='.' && inputValue=='.'){
+    return false;
+  }else if(operators.includes(inputValue)){
+    
+    if(operators.includes(lastInput)){
+      return false;
+    }else{
+      return true;
+    }
+  }else{
+    return true;
+  }
+
+} 
